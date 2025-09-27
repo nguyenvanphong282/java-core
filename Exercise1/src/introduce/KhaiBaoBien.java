bc02d82764de166ac2a5fc7fae3f74e7ec956bba
nguyenvanphong282 <kincoderc@gmail.com>
2025-09-28 00:37:49 +0700

    commit cach KhaiBaoBien

---
+++ Exercise1/src/introduce/KhaiBaoBien.java
@@ -0,0 +1,19 @@
+package introduce;
+
+public class KhaiBaoBien {
+    public static void main(String[] args) {
+        int a=3;
+        int b=2;
+        int c = a+b;
+        System.out.println("Tong la: "+ c);
+        System.out.println("-------------------------------");
+        String name = "Phong";
+        String ho = "Nguyen";
+        String dem = "Van";
+        System.out.println(ho+" "+dem+" "+name);
+        System.out.println("----------------------------------");
+        String hello;
+        hello = "Xin chao moi nguoi!";
+        System.out.println(hello);
+    }
+}
