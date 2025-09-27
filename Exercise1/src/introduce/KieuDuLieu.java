47fb78b2f6b46de263841f1fe692fa5dbc9b3413
nguyenvanphong282 <kincoderc@gmail.com>
2025-09-28 00:46:11 +0700

    Tim hieu ve kieu du lieu

---
+++ Exercise1/src/introduce/KieuDuLieu.java
@@ -0,0 +1,16 @@
+package introduce;
+
+public class KieuDuLieu {
+    public static void main(String[] args) {
+        int tuoi = 20;
+        String hoVaTen = "Nguyen Van Phong";
+        double dtb = 5.0;
+        char quyDoi = 'D';
+        boolean homNayDiHoc = true;
+        System.out.println("Họ và tên: "+hoVaTen);
+        System.out.println("Tuổi: "+tuoi);
+        System.out.println("Điểm trung bình hệ 10: " + dtb);
+        System.out.println("Điểm quy đổi: "+ quyDoi);
+        System.out.println("Hôm nay có đi học: "+homNayDiHoc);
+    }
+}
