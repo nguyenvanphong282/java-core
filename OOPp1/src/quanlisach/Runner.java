ce342823e1dae6d8829e9c0c878cd665bd6e680b
nguyenvanphong282 <kincoderc@gmail.com>
2025-09-30 15:37:49 +0700

    commit

--- OOPp1/src/quanlisach/Runner.java
+++ OOPp1/src/quanlisach/Runner.java
@@ -5,7 +5,10 @@ public class Runner {
         Ngay n1 = new Ngay(1,1,1999);
         TacGia tacGia1 = new TacGia("PhongIT",n1);
         Sach s1 = new Sach("Lap trinh Java",tacGia1,400,1990);
+        Sach s2 = new Sach("Lap trinh C/C++",tacGia1,300,1990);
         System.out.println(s1.getTenSach());
         System.out.println("Gia sach "+s1.getTenSach()+" la: "+s1.getGiaSach());
+        System.out.println("------------------");
+        System.out.println("Kiem tra nam san xuat trung nhau: "+s1.checkTrungNamXuatBan(s2));
     }
 }
